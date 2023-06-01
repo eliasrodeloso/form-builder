e77e48d1d650277b4d82f44de9ffd1d3f7d331c0
Elias Rodelo <eliasrodeloso@gmail.com>
2023-06-01 15:26:51 -0500

    feat(console): added console components

--- src/features/console/components/index.tsx
+++ src/features/console/components/index.tsx
@@ -1,3 +1,52 @@
+import { useSyncExternalStore, type FormEvent } from "react";
+import { Box, Button, chakra, Grid, Input } from "@chakra-ui/react";
+
+import { addCommand, commandStore } from "~/core/store/commands";
+
 export function Console() {
-  return <div>Console</div>;
+  const commands = useSyncExternalStore(
+    commandStore.subscribe,
+    commandStore.getState,
+    commandStore.getState
+  );
+
+  const formSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+
+    const data = new FormData(form);
+    const command = data.get("commands") as string;
+
+    addCommand(command);
+    form.reset();
+  };
+
+  return (
+    <Grid
+      height="100%"
+      templateAreas={`"prompt" "input"`}
+      gridTemplateRows="1fr 100px"
+    >
+      <Box p={2}>
+        {commands.map((command) => (
+          <div key={command.id}>
+            <p>{command.input}</p>
+          </div>
+        ))}
+      </Box>
+      <Box display="flex" alignItems="flex-end" mx={-4} my={-2}>
+        <chakra.form onSubmit={formSubmitHandler} display="flex" width="100%">
+          <Input type="text" name="commands" borderRadius="0" />
+          <Button
+            type="submit"
+            backgroundColor="black"
+            color="white"
+            borderRadius="0"
+          >
+            Run
+          </Button>
+        </chakra.form>
+      </Box>
+    </Grid>
+  );
 }
