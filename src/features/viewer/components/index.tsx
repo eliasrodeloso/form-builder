27d315bf43ae9937736b1035af843703f35f7ba7
Elias Rodelo <eliasrodeloso@gmail.com>
2023-06-05 16:47:52 -0500

    feat(viewer): updated view to consume model data

--- src/features/viewer/components/index.tsx
+++ src/features/viewer/components/index.tsx
@@ -1,20 +1,20 @@
 import { useSyncExternalStore } from "react";
 import { Box, Container } from "@chakra-ui/react";
 
-import { commandStore } from "~/core/store/commands";
+import { commandsStore } from "~/core/store/commands";
 import { commandMapper } from "~/features/viewer/helpers/commandMapper";
 
 export function Viewer() {
-  const commandsStore = useSyncExternalStore(
-    commandStore.subscribe,
-    commandStore.getState,
-    commandStore.getState
+  const commandsState = useSyncExternalStore(
+    (state) => commandsStore.subscribe(state),
+    () => commandsStore.getState(),
+    () => commandsStore.getState()
   );
 
   return (
     <Container maxW="550" minW="300">
       <Box my={24}>
-        {commandsStore.map((command) => {
+        {commandsState.map((command) => {
           const executor = commandMapper[command.type];
 
           if (!executor) {
