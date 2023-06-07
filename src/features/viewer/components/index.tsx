e36d3ed9d1025bc527c2aefabf067acd688bb2a1
Elias Rodelo <eliasrodeloso@gmail.com>
2023-06-06 19:23:30 -0500

    feat(viewer): refactor viewer to use new command model

--- src/features/viewer/components/index.tsx
+++ src/features/viewer/components/index.tsx
@@ -1,29 +1,20 @@
 import { useSyncExternalStore } from "react";
 import { Box, Container } from "@chakra-ui/react";
 
-import { commandsStore } from "~/core/store/commands";
-import { commandMapper } from "~/features/viewer/helpers/commandMapper";
+import { commands } from "~/core/store/commands";
 
 export function Viewer() {
-  const commandsState = useSyncExternalStore(
-    (state) => commandsStore.subscribe(state),
-    () => commandsStore.getState(),
-    () => commandsStore.getState()
+  const appState = useSyncExternalStore(
+    (listener) => commands.subscribe(listener),
+    () => commands.getViewStateSnapshot(),
+    () => commands.getViewStateSnapshot()
   );
 
   return (
     <Container maxW="550" minW="300">
       <Box my={24}>
-        {commandsState.map((command) => {
-          const executor = commandMapper[command.type];
-
-          if (!executor) {
-            return null;
-          }
-
-          const Component = executor(command.args);
-
-          return <Component key={`${command.type}-component-${command.id}`} />;
+        {appState.map(({ viewType: ViewType, ...view }) => {
+          return <ViewType key={`component-${view.id}`} {...view} />;
         })}
       </Box>
     </Container>
